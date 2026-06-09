bbe5108c7bb9a904d0edcf3c03c2c34f2d2ebda7
sh990920 <sh990920@gmail.com>
2026-06-09 12:01:02 +0900

    first commit

---
+++ src/main/java/com/ppip/dallyeo/DallyeoApplication.java
@@ -0,0 +1,13 @@
+package com.ppip.dallyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DallyeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DallyeoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ppip/dallyeo/DallyeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppip.dallyeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DallyeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
